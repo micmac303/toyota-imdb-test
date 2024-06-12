d9a3264383d7afcde0313d5d9bed247696925417
Michael McCarthy <michaelmccarthy@Michaels-MacBook-Air.local>
2024-06-12 23:47:33 +0100

    Skeleton application code

---
+++ src/test/java/com/timeless/toyotaimdbtest/ToyotaImdbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeless.toyotaimdbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyotaImdbTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
