c696afcd0cb5f7bf60053b5977f85301df2dbdab
Michael McCarthy <michaelmccarthy@Michaels-MacBook-Air.local>
2024-06-13 00:00:51 +0100

    CommandLineRunner

--- src/main/java/com/timeless/toyotaimdbtest/ToyotaImdbTestApplication.java
+++ src/main/java/com/timeless/toyotaimdbtest/ToyotaImdbTestApplication.java
@@ -1,13 +1,22 @@
 package com.timeless.toyotaimdbtest;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ToyotaImdbTestApplication {
+@SpringBootApplication @Slf4j
+public class ToyotaImdbTestApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
+		log.info("Starting the application");
 		SpringApplication.run(ToyotaImdbTestApplication.class, args);
+		log.info("Application finished");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Application started with command-line argument: {}", args[0]);
+	}
 }
