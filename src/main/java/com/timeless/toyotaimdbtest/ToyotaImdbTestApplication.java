6c31c78f72761171a87ff4366af753849e8a971d
Michael McCarthy <micmac303@gmail.com>
2024-06-12 23:50:59 +0100

    IMDB1-Skeleton-Application (#1)
    * Skeleton application
    
    * Skeleton application code
    
    ---------
    
    Co-authored-by: Michael McCarthy <michaelmccarthy@Michaels-MacBook-Air.local>

---
+++ src/main/java/com/timeless/toyotaimdbtest/ToyotaImdbTestApplication.java
@@ -0,0 +1,13 @@
+package com.timeless.toyotaimdbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyotaImdbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyotaImdbTestApplication.class, args);
+	}
+
+}
